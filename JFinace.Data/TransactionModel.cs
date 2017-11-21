b67e7f15a3fc24ed90604b8885c9142aff776b36
Jagreaper <jamesalexgrant@hotmail.com>
2017-11-21 18:00:48 +1000

    Added SummaryView and TransactionModel class

---
+++ JFinace.Data/TransactionModel.cs
@@ -0,0 +1,71 @@
+﻿using JFinace.Mvvm;
+using System;
+using System.Collections.ObjectModel;
+
+namespace JFinace.Data
+{
+    public enum TransactionType
+    {
+        Credit,
+        Debit,
+    }
+
+    public class TransactionModel : ObservableObject
+    {
+        #region Fields
+
+        private ObservableCollection<string> tags;
+
+        private string category;
+
+        public TransactionType transactionType;
+
+        public double amount;
+
+        public DateTime timestamp;
+
+        public string description;
+
+        #endregion
+
+        #region Properties
+
+        public ObservableCollection<string> Tags
+        {
+            get => this.tags;
+            set => this.Set(ref this.tags, value);
+        }
+
+        public string Category
+        {
+            get => this.category;
+            set => this.Set(ref this.category, value);
+        }
+
+        public TransactionType TransactionType
+        {
+            get => this.transactionType;
+            set => this.Set(ref this.transactionType, value);
+        }
+
+        public double Amount
+        {
+            get => this.amount;
+            set => this.Set(ref this.amount, value);
+        }
+
+        public DateTime Timestamp
+        {
+            get => this.timestamp;
+            set => this.Set(ref this.timestamp, value);
+        }
+
+        public string Description
+        {
+            get => this.description;
+            set => this.Set(ref this.description, value);
+        }
+
+        #endregion
+    }
+}
