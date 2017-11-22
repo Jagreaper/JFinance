47dd2a51fad1a22db29b301eb2b0d571dbd2f396
Jagreaper <jamesalexgrant@hotmail.com>
2017-11-22 11:54:36 +1000

    Added Summary Button

--- JFinace.Windows.WPF/ViewModels/MainViewModel.cs
+++ JFinace.Windows.WPF/ViewModels/MainViewModel.cs
@@ -1,13 +1,58 @@
 ﻿using JFinace.Mvvm;
+using JFinace.Windows.WPF.Views;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace JFinace.Windows.WPF.ViewModels
 {
     class MainViewModel : ViewModel
     {
+        #region Constructor
+
+        public MainViewModel()
+        {
+            this.NavigateCommand = new RelayCommand<string>(this.Navigate);
+            this.ContentControl = new SummaryView();
+        }
+
+        #endregion
+
+        #region Fields
+
+        public object contentControl;
+
+        #endregion
+
+        #region Properties
+
+        public ICommand NavigateCommand { get; private set; }
+
+        public object ContentControl
+        {
+            get => this.contentControl;
+            set => this.Set(ref this.contentControl, value);
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void Navigate(string arg)
+        {
+            switch(arg)
+            {
+                case "NavigateSummary":
+                    this.ContentControl = new SummaryView();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        #endregion
     }
 }
--- JFinace.Windows.WPF/Views/SummaryView.xaml.cs
+++ JFinace.Windows.WPF/Views/SummaryView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using JFinace.Windows.WPF.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,7 +23,8 @@ namespace JFinace.Windows.WPF.Views
     {
         public SummaryView()
         {
-            InitializeComponent();
+            this.InitializeComponent();
+            this.DataContext = new SummaryViewModel();
         }
     }
 }
