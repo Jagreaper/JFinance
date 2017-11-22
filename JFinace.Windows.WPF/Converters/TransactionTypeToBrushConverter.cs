d543efd6c35f43fce2d7cdc549f9cc81b30adcd6
Jagreaper <jamesalexgrant@hotmail.com>
2017-11-22 15:36:27 +1000

    Added Converters for TransactionModel type in the Summary ListBox

---
+++ JFinace.Windows.WPF/Converters/TransactionTypeToBrushConverter.cs
@@ -0,0 +1,42 @@
+﻿using JFinace.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace JFinace.Windows.WPF.Converters
+{
+    class TransactionTypeToBrushConverter : IValueConverter
+    {
+        #region Methods
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is TransactionType))
+                throw new NotSupportedException();
+
+            switch ((TransactionType)value)
+            {
+                case TransactionType.Credit:
+                    return Brushes.Green;
+                case TransactionType.Debit:
+                    return Brushes.Red;
+                case TransactionType.None:
+                    return Brushes.Gray;
+                default:
+                    throw new NotSupportedException();
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
