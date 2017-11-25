c5c1ff57fdd34603d1b748ade4f8b31ad946b844
Jagreaper <jamesalexgrant@hotmail.com>
2017-11-25 15:00:43 +1000

    Fixed Information tooltip

---
+++ JFinance.Windows.WPF/Converters/CategoryNamePiePlotterConverter.cs
@@ -0,0 +1,42 @@
+﻿using JFinance.Windows.WPF.Controls;
+using JFinance.Windows.WPF.Helpers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+using static JFinance.Windows.WPF.ViewModels.GraphViewModel;
+
+namespace JFinance.Windows.WPF.Converters
+{
+    [ValueConversion(typeof(object), typeof(string))]
+    public class CategoryNamePiePlotterConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            PiePlotter plotter = null;
+            PieElement element = null;
+
+            foreach (object value in values)
+            {
+                if (value is PiePlotter)
+                    plotter = (PiePlotter)value;
+
+                if (value is PieElement)
+                    element = (PieElement)value;
+            }
+            return TypeDescriptor.GetProperties(element)[plotter.CategoryName].GetValue(element);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
